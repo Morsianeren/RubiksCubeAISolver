3a1599714ddb0f0d3121873dd9d7bb27c6f1c5e2
Magnus Klokker <magnusklokker@hotmail.com>
2024-02-26 17:11:00 +0100

    Pieces now inherits from a super piece

--- RubiksCubeSimulation/Cube.py
+++ RubiksCubeSimulation/Cube.py
@@ -1,31 +1,32 @@
 #%%
 import numpy as np
 from typing import Literal
+from abc import ABC, abstractmethod
 
-class EdgePiece():
-        def __init__(self):
-            pass
 
-        def __str__(self):
-            return "E"
+class Piece():
+    def __init__(self, letter, number: int = None):
+        self.number = number
+        self.letter = letter
 
-class CornerPiece():
-    def __init__(self):
-        pass
     def __str__(self):
-        return "C"
+        return str(self.number) if self.number is not None else self.letter
 
-class MiddlePiece():
-    def __init__(self):
-        pass
-    def __str__(self):
-        return "M"
+class EdgePiece(Piece):
+        def __init__(self, number: int = None):
+            super().__init__("E", number)
+
+class CornerPiece(Piece):
+    def __init__(self, number: int = None):
+            super().__init__("C", number)
+
+class MiddlePiece(Piece):
+    def __init__(self, number: int = None):
+            super().__init__("M", number)
     
-class CorePiece():
-    def __init__(self) -> None:
-        pass
-    def __str__(self):
-        return "X"
+class CorePiece(Piece):
+    def __init__(self, number: int = None):
+            super().__init__("X", number)
 
 class Cube():
     def __init__(self):
@@ -41,6 +42,8 @@ class Cube():
             # Classify the piece
             self.cube[x, y, z] = self._classify_piece(x, y, z)
 
+        self.org_cube = self.cube.copy() # Save the original state of the cube
+
     def __str__(self):
         counter = 1
         string = ""
@@ -54,6 +57,24 @@ class Cube():
             counter += 1
 
         return string
+    
+    def rotate_center(self, axis: Literal["x", "y", "z"], direction: Literal["cw", "ccw"]):
+        """Function to rotate the center of the cube like a rubiks cube
+
+        Args:
+            axis (Literal["x", "y", "z"]): Which axis to rotate
+            direction (Literal["cw", "ccw"]): Direction of the rotation. Clockwise (cw) or counter clockwise (ccw)
+        """
+        axis_dict = {
+            "x": (1, 2),
+            "y": (0, 2),
+            "z": (0, 1)
+        }
+        self.cube = np.rot90(self.cube, axes=axis_dict[axis], k=1 if direction == "cw" else -1)
+
+
+    def rotate_edge(self):
+        pass
 
     def _classify_piece(self, x, y, z):
         ones_count = sum(x == 1 for x in (x, y, z))
