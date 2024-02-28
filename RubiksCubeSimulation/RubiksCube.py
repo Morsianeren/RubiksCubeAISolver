1d8025efada9538e9c2b9bb4ae5fb8d4d9e64172
Magnus Klokker <magnusklokker@hotmail.com>
2024-02-28 14:46:29 +0100

    Initial quaternion script

---
+++ RubiksCubeSimulation/RubiksCube.py
@@ -0,0 +1,6 @@
+# In this class i will use quaternions to keep track of the orientation of the pieces!
+
+# " Quaternions came from Hamilton after his really good work had been done; 
+# and, though beautifully ingenious, 
+# have been an unmixed evil to those who have touched them in any way, including Clerk Maxwell"
+# - Lord Kelvin
