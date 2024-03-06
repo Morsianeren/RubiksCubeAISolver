e15e32af99c028344acb10f4cde3c72c17a13815
Magnus Klokker <magnusklokker@hotmail.com>
2024-03-06 11:50:43 +0100

    Installs requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
+
+with open('requirements.txt', encoding='UTF-8') as f:
+    requirements = f.read().splitlines()
+
+
 setup(
-    name = 'RubiksCubeSimulation',
+    name = 'RubiksCube',
     packages = find_packages(),
+    install_requires = requirements,
 )
