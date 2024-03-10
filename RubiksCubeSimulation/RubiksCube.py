277b3b61ae3b9436e21bf75307b6384874bf86d3
Magnus Klokker <magnusklokker@hotmail.com>
2024-03-10 18:15:57 +0100

    Initial program

--- RubiksCubeSimulation/RubiksCube.py
+++ RubiksCubeSimulation/RubiksCube.py
@@ -4,3 +4,21 @@
 # and, though beautifully ingenious, 
 # have been an unmixed evil to those who have touched them in any way, including Clerk Maxwell"
 # - Lord Kelvin
+
+# %% 
+from Piece import Piece
+import matplotlib.pyplot as plt
+
+# Create a array with coordinates for all the pieces
+pieces = []
+for x in range(-1, 2):
+    for y in range(-1, 2):
+        for z in range(-1, 2):
+            pieces.append(Piece(x, y, z))
+
+# Plot the pieces
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for piece in pieces:
+    piece.plot(ax)
