2c9d1c6147410877b278434bfa99a29a03aa4eaa
Magnus Klokker <magnusklokker@hotmail.com>
2024-03-10 17:35:36 +0100

    Rounds xyz vectors

--- RubiksCubeSimulation/Quaternions/Quaternion.py
+++ RubiksCubeSimulation/Quaternions/Quaternion.py
@@ -104,13 +104,9 @@ class Quaternion():
     def plot(self, **kwargs):
         # This function plots the quaternion as a vector in 3D space
         x, y, z = self.get_xyz_vectors()
-        #x = self.x
-        #y = self.y
-        #z = self.z
-        #plt.quiver(0, 0, 0, x, y, z, **kwargs)
-        plt.quiver(0, 0, 0, x[0], x[1], x[2], color='r')
-        plt.quiver(0, 0, 0, y[0], y[1], y[2], color='g')
-        plt.quiver(0, 0, 0, z[0], z[1], z[2], color='b')
+        plt.quiver(0, 0, 0, x[0], x[1], x[2], color='r', **kwargs)
+        plt.quiver(0, 0, 0, y[0], y[1], y[2], color='g', **kwargs)
+        plt.quiver(0, 0, 0, z[0], z[1], z[2], color='b', **kwargs)
 
 def qv_multiply(quat: Quaternion, vector: np.array) -> Quaternion:
     q1 = quat
@@ -229,6 +225,11 @@ def get_direction_vectors(quat: Quaternion):
     x_vector = rotation_matrix[:, 0]  # Vector along x-axis
     y_vector = rotation_matrix[:, 1]  # Vector along y-axis
     z_vector = rotation_matrix[:, 2]  # Vector along z-axis
+
+    x_vector = np.round(x_vector, 5)
+    y_vector = np.round(y_vector, 5)
+    z_vector = np.round(z_vector, 5)
+
     return x_vector, y_vector, z_vector
 
 # Test code
