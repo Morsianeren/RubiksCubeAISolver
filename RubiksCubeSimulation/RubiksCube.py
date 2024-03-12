331b029ff24f45f5c248480e94876bb05970f3dd
TrunteDK <morsianeren@gmail.com>
2024-03-12 14:12:10 +0100

    Code assembled into a RubiksCube class

--- RubiksCubeSimulation/RubiksCube.py
+++ RubiksCubeSimulation/RubiksCube.py
@@ -11,79 +11,82 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import Literal
 
-POSITION_SCALE = 4
+class RubiksCube():
+    POSITION_SCALE = 4
 
-pieces = np.zeros((3, 3, 3), dtype=Piece)
+    def __init__(self):
+        pieces = np.zeros((3, 3, 3), dtype=Piece)
 
-# Initialize the pieces
-# The cube will have center in origo
-for x, y, z in np.ndindex(3, 3, 3):
-    pieces[x, y, z] = Piece((x-1)*POSITION_SCALE, 
-                            (y-1)*POSITION_SCALE, 
-                            (z-1)*POSITION_SCALE)
+        # Initialize the pieces
+        # The cube will have center in origo
+        for x, y, z in np.ndindex(3, 3, 3):
+            pieces[x, y, z] = Piece((x-1)*self.POSITION_SCALE,
+                                    (y-1)*self.POSITION_SCALE,
+                                    (z-1)*self.POSITION_SCALE)
 
-# Assign colors to the pieces
-for row in pieces[0,:,:]:
-    for piece in row:
-        piece.colors['-x'] = "yellow"
+        # Assign colors to the pieces
+        for row in pieces[0,:,:]:
+            for piece in row:
+                piece.colors['-x'] = "yellow"
 
-for row in pieces[2,:,:]:
-    for piece in row:
-        piece.colors['x'] = "white"
+        for row in pieces[2,:,:]:
+            for piece in row:
+                piece.colors['x'] = "white"
 
-for row in pieces[:,0,:]:
-    for piece in row:
-        piece.colors['-y'] = "orange"
+        for row in pieces[:,0,:]:
+            for piece in row:
+                piece.colors['-y'] = "orange"
 
-for row in pieces[:,2,:]:
-    for piece in row:
-        piece.colors['y'] = "red"
+        for row in pieces[:,2,:]:
+            for piece in row:
+                piece.colors['y'] = "red"
 
-for row in pieces[:,:,0]:
-    for piece in row:
-        piece.colors['-z'] = "green"
+        for row in pieces[:,:,0]:
+            for piece in row:
+                piece.colors['-z'] = "green"
 
-for row in pieces[:,:,2]:
-    for piece in row:
-        piece.colors['z'] = "blue"
+        for row in pieces[:,:,2]:
+            for piece in row:
+                piece.colors['z'] = "blue"
 
-# Plot the pieces
-style='square'
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+        # After this there is no need for the array to be 3x3x3
+        # since rotation will mess up the indexing
+        self.pieces = pieces.flatten()
 
-for piece in pieces.flatten():
-    piece.plot(ax, style)
+    def plot(self, style:Literal['square', 'arrows']='square'):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
 
-ax.set_xlim(-8, 8)
-ax.set_ylim(-8, 8)
-ax.set_zlim(-8, 8)
-plt.show()
+        for piece in self.pieces:
+            piece.plot(ax, style)
 
-# After this there is no need for the array to be 3x3x3
-# since rotation will mess up the indexing
-pieces = pieces.flatten()
+        ax.set_xlim(-8, 8)
+        ax.set_ylim(-8, 8)
+        ax.set_zlim(-8, 8)
+        plt.show()
 
-def rotate_side(pieces:list, axis:Literal['x', 'y', 'z'], row:int, k:int):
-    lower_position = -POSITION_SCALE
-    middle_position = 0
-    upper_position = POSITION_SCALE
+    def rotate_side(self, axis:Literal['x', 'y', 'z'], row:int, k:int):
+        lower_position = -self.POSITION_SCALE
+        middle_position = 0
+        upper_position = self.POSITION_SCALE
 
-    if row not in [lower_position, upper_position, middle_position]:
-        raise ValueError(f"Row must be one of {lower_position}, {middle_position}, or {upper_position}, got {row}.")
-    # Filter out the pieces that should be rotated
-    idx = 'xyz'.index(axis)
-    pieces_to_rotate = [piece for piece in pieces if piece.position[idx] == row]
+        pieces = self.pieces
 
-    # Rotate pieces
-    rotated_pieces = rotate_pieces(pieces_to_rotate, axis, k)
+        if row not in [lower_position, upper_position, middle_position]:
+            raise ValueError(f"Row must be one of {lower_position}, {middle_position}, or {upper_position}, got {row}.")
+        # Filter out the pieces that should be rotated
+        idx = 'xyz'.index(axis)
+        pieces_to_rotate = [piece for piece in pieces if piece.position[idx] == row]
 
-    # Assemble the rotated pieces back the original list
-    for i, piece in enumerate(pieces):
-        if piece in pieces_to_rotate:
-            pieces[i] = rotated_pieces.pop(0)
+        # Rotate pieces
+        rotated_pieces = rotate_pieces(pieces_to_rotate, axis, k)
 
-    return pieces
+        # Assemble the rotated pieces back the original list
+        for i, piece in enumerate(pieces):
+            if piece in pieces_to_rotate:
+                pieces[i] = rotated_pieces.pop(0)
+
+        return pieces
 
 def rotate_pieces(pieces: list, axis:Literal['x', 'y', 'z'], k:int):
     """Rotates all given pieces k*90 degrees around the given axis.
@@ -103,17 +106,11 @@ def rotate_pieces(pieces: list, axis:Literal['x', 'y', 'z'], k:int):
         #piece.rotate(axis)
     return pieces_copy
 
-# %% Rotate the cube
-# Rotate the cube 90 degrees around the x-axis
-pieces = rotate_side(pieces, 'y', 0, 1)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-for piece in pieces:
-    piece.plot(ax, style)
-
-ax.set_xlim(-8, 8)
-ax.set_ylim(-8, 8)
-ax.set_zlim(-8, 8)
-plt.show()
+# %% Test code
+# cube = RubiksCube()
+# 
+# cube.plot()
+# 
+# cube.rotate_side('x', 0, 1)
+# 
+# cube.plot()
